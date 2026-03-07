c109b074a19e4c0f01e2cc82221d61a6b60dd17e
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-03-07 13:02:21 +0200

    Add files via upload

--- control.js
+++ control.js
@@ -7,8 +7,8 @@ var APK_REQUIRED_VERSION = "1.0";
 var APK_DOWNLOAD_LINK = "https://www.mediafire.com/file/wavgumgx0e4xrk5/Manzoma+Pro33.apk/file"; 
 
 // 🟢 2. رقم إصدار البرامج (التحديث الهوائي الداخلي - OTA)
-// ⚠️ تم التحديث إلى 122 لسحب التعديلات الجديدة
-var HTML_VERSION = "122"; 
+// ⚠️ تم التحديث إلى 125 لسحب التعديلات الجديدة (ميزة المشاركة Share API)
+var HTML_VERSION = "125"; 
 
 // 🔵 3. روابط البرامج (يجب أن تظل كما هي للعمل أوفلاين)
 var remoteApps = [
