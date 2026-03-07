90d4ce06bf2b331d9a31fb82e891ae3a1b6f9a6d
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-03-07 02:41:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,16 @@
 const CACHE_NAME = 'manzoma-cache-v1';
 
-// لما التطبيق يطلب أي ملف، هنجيبه من الكاش الأول عشان يشتغل أوفلاين
+// 1. التثبيت الإجباري للتحديثات الجديدة فوراً (عشان ميعلقش على النسخة القديمة)
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+// 2. تفعيل السيطرة على التطبيق فوراً بمجرد فتح البرنامج
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim()); 
+});
+
+// 3. لما التطبيق يطلب أي ملف، هنجيبه من الكاش الأول عشان يشتغل أوفلاين
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
