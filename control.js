e96214c160a4c911d4d2ad18c71ebe628fb40488
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-03-26 19:16:27 +0200

    Add files via upload

--- control.js
+++ control.js
@@ -1,3 +1,3 @@
 var APK_REQUIRED_VERSION = "1.0"; 
-var HTML_VERSION = "20"; 
+var HTML_VERSION = "22"; // ارفع الرقم لـ 22 ليتم سحب نسخة الاستقرار الجديدة
 var APK_DOWNLOAD_LINK = "https://minaibrahimfawzy-droid.github.io/Manzoma-pro/app.apk";
--- sw.js
+++ sw.js
@@ -11,13 +11,29 @@ const urlsToCache = [
   './control.js',
   './sw.js'
 ];
+
+// تثبيت الملفات في الكاش فوراً
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
+
+// استراتيجية التشغيل: ابحث في الكاش أولاً للأوفلاين
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
+
+// تنشيط وحذف الكاش القديم
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
 });
