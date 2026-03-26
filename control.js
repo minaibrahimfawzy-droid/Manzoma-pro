fcf0cc43200beb38b9fe5751b7bfd397a809eede
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-03-26 10:27:43 +0200

    Add files via upload

--- control.js
+++ control.js
@@ -1,3 +1,3 @@
 var APK_REQUIRED_VERSION = "1.0"; 
-var HTML_VERSION = "17"; 
+var HTML_VERSION = "18"; 
 var APK_DOWNLOAD_LINK = "https://minaibrahimfawzy-droid.github.io/Manzoma-pro/app.apk";
--- sw.js
+++ sw.js
@@ -11,27 +11,13 @@ const urlsToCache = [
   './control.js',
   './sw.js'
 ];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Cache Created');
-      return cache.addAll(urlsToCache);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
