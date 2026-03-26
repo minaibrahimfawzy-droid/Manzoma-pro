1dc4a7e13b246cd276f5cb9a96d2abda78b5cca0
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-03-26 16:30:30 +0200

    Add files via upload

--- control.js
+++ control.js
@@ -1,3 +1,3 @@
 var APK_REQUIRED_VERSION = "1.0"; 
-var HTML_VERSION = "19"; // ارفع الرقم لـ 19 ليتم سحب نسخة الأوفلاين لكل الأجهزة
+var HTML_VERSION = "20"; 
 var APK_DOWNLOAD_LINK = "https://minaibrahimfawzy-droid.github.io/Manzoma-pro/app.apk";
--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = 'manzoma-cache-v8';
 const urlsToCache = [
-  './',
   './index.html',
   './Nabatshia.html',
   './2.html',
@@ -12,30 +11,13 @@ const urlsToCache = [
   './control.js',
   './sw.js'
 ];
-
-// التثبيت: سحب الملفات وتخزينها
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('--- حماية الأوفلاين نشطة ---');
-      return cache.addAll(urlsToCache);
-    })
-  );
-  self.skipWaiting(); // تفعيل فوري للنسخة الجديدة
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
-
-// التشغيل: استراتيجية الكاش أولاً للأوفلاين
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
-
-// التحديث: حذف الكاش القديم
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
