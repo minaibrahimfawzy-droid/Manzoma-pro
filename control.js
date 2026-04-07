92c1283d704fa5676c846597fae7ae767ac148ff
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-04-07 13:20:59 +0200

    Add files via upload

--- control.js
+++ control.js
@@ -1,3 +1,3 @@
 var APK_REQUIRED_VERSION = "1.0"; 
-var HTML_VERSION = "24"; 
+var HTML_VERSION = "25"; 
 var APK_DOWNLOAD_LINK = "https://minaibrahimfawzy-droid.github.io/Manzoma-pro/app.apk";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'manzoma-cache-v8';
+const CACHE_NAME = 'manzoma-cache-v9';
 const urlsToCache = [
   './',
   './index.html',
