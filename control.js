75821080db69496fdf93dddfadde03366a8b4699
minaibrahimfawzy-droid <minaibrahimfawzy@gmail.com>
2026-04-20 11:19:55 +0200

    Add files via upload

--- control.js
+++ control.js
@@ -1,5 +1,4 @@
 --- START OF FILE control.js ---
-
 var APK_REQUIRED_VERSION = "1.0"; 
-var HTML_VERSION = "30"; 
+var HTML_VERSION = "50"; // قفزة في الإصدار من 35 إلى 50 لضمان السماع
 var APK_DOWNLOAD_LINK = "https://minaibrahimfawzy-droid.github.io/Manzoma-pro/app.apk";
--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 --- START OF FILE sw.js ---
-
-const CACHE_NAME = 'manzoma-cache-v14';
+const CACHE_NAME = 'manzoma-cache-v25'; // قفزة في الإصدار لضمان التحديث
 const urlsToCache = [
   './',
   './index.html',
@@ -16,15 +15,29 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
   return self.clients.claim();
 });
